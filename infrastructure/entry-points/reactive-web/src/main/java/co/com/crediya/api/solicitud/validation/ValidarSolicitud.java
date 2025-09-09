43e3ca91a918e31184db418a884f6bdb7dae3edc
BorisMejia <borisfernandez916@gmail.com>
2025-09-08 19:02:45 -0500

    feat: Add missing @Component annotation to ValidarSolicitud class for proper Spring context management

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/solicitud/validation/ValidarSolicitud.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/solicitud/validation/ValidarSolicitud.java
@@ -2,11 +2,13 @@ package co.com.crediya.api.solicitud.validation;
 
 import co.com.crediya.api.solicitud.dto.request.RegistroSolicitudRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.SmartValidator;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
+@Component
 public class ValidarSolicitud {
 
     private final SmartValidator validator;
