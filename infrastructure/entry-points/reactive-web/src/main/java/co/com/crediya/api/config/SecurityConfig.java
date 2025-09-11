a4154bc02357e7587d254f114b88998caa63324e
BorisMejia <borisfernandez916@gmail.com>
2025-09-11 14:24:05 -0500

    feat: Add custom authentication and access denied handlers for improved security responses

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/config/SecurityConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package co.com.crediya.api.config;
 
 
+import co.com.crediya.api.security.securityHandlers.SecurityHandlers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -13,12 +13,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
-import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServerBearerExchangeFilterFunction;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -65,6 +60,10 @@ public class SecurityConfig {
                 .oauth2ResourceServer(o -> o
                         .jwt(j -> j.jwtAuthenticationConverter(jwtConverter))
                 )
+                .exceptionHandling(exceptionHandlingSpec ->
+                        exceptionHandlingSpec
+                                .authenticationEntryPoint(SecurityHandlers.authenticationEntryPoint())
+                                .accessDeniedHandler(SecurityHandlers.accessDeniedHandler()))
                 .build();
     }
 }
