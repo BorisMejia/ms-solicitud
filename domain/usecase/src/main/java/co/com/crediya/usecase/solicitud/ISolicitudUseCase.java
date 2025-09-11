15fa3804ee5a5e0d44586f34fd77239fc13e0714
BorisMejia <borisfernandez916@gmail.com>
2025-09-11 13:36:45 -0500

    feat: Implement pagination in ISolicitudUseCase and SolicitudUseCase classes

--- domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/ISolicitudUseCase.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/ISolicitudUseCase.java
@@ -1,6 +1,8 @@
 package co.com.crediya.usecase.solicitud;
 
 import co.com.crediya.model.solicitud.Solicitud;
+import co.com.crediya.model.solicitud.pagination.PageQuery;
+import co.com.crediya.model.solicitud.pagination.PageResult;
 import co.com.crediya.usecase.solicitud.dto.request.SolicitudUseCaseDto;
 import reactor.core.publisher.Mono;
 
@@ -8,4 +10,5 @@ import java.math.BigDecimal;
 
 public interface ISolicitudUseCase {
     Mono<Solicitud> registrarSolicitud(SolicitudUseCaseDto solicitud);
+    Mono<PageResult<Solicitud>> ejecutarPagination(PageQuery query);
 }
