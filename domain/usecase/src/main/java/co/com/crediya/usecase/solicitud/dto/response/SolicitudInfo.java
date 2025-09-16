0ec3a1b1d1d78b7ad6dd7d9383796660b34f9d71
BorisMejia <borisfernandez916@gmail.com>
2025-09-15 22:31:09 -0500

    feat: Add monto_mensual_solicitud field to SolicitudInfo for comprehensive financial details

--- domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/dto/response/SolicitudInfo.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/dto/response/SolicitudInfo.java
@@ -12,5 +12,6 @@ public record SolicitudInfo(
     String nombre_estado_solicitud,
     String nombre_solicitante,
     Double salario_base,
-    BigDecimal tasa_interes
+    BigDecimal tasa_interes,
+    BigDecimal monto_mensual_solicitud
 ) {}
