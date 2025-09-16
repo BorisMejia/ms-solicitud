0e0620a425b987ca27581317297cdac58782297d
BorisMejia <borisfernandez916@gmail.com>
2025-09-15 21:48:30 -0500

    feat: Add tasa_interes field to SolicitudInfo for enhanced financial details

--- domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/dto/response/SolicitudInfo.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/dto/response/SolicitudInfo.java
@@ -11,5 +11,6 @@ public record SolicitudInfo(
     String nombre_tipo_prestamo,
     String nombre_estado_solicitud,
     String nombre_solicitante,
-    Double salario_base
+    Double salario_base,
+    BigDecimal tasa_interes
 ) {}
