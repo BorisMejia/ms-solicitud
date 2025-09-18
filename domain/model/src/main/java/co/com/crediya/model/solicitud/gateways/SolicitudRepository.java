1aaa3f0add10ec2c73c6ba2fc9e92fe9952ec9c2
BorisMejia <borisfernandez916@gmail.com>
2025-09-18 00:27:54 -0500

    feat: Add updateEstadoSolicitud method to SolicitudRepository interface

--- domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
@@ -1,5 +1,6 @@
 package co.com.crediya.model.solicitud.gateways;
 
+import co.com.crediya.model.estado.EstadoSolicitud;
 import co.com.crediya.model.solicitud.Solicitud;
 import co.com.crediya.model.solicitud.pagination.PageQuery;
 import co.com.crediya.model.solicitud.pagination.PageResult;
@@ -8,4 +9,5 @@ import reactor.core.publisher.Mono;
 public interface SolicitudRepository {
     Mono<Solicitud> saveSolicitud(Solicitud solicitud);
     Mono<PageResult<Solicitud>> findPage(PageQuery query);
+    Mono<Solicitud> updateEstadoSolicitud(Long idSolicitud, EstadoSolicitud nuevoEstado);
 }
